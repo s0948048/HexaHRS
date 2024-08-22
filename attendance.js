0dcbfe18a2981300db7c5e487fe0a1a752b31571
s0948048 <chenti6508@gmail.com>
2024-08-22 15:53:55 +0800

    完成假勤考勤所有資料，準備排版

---
+++ attendance.js
@@ -0,0 +1,9 @@
+// const atn_index = document.getElementById('atn_list');
+// const atn_except = document.getElementById('see_except_data');
+
+
+
+
+
+// atn_index.addEventListener('click',()=>{loadPage('attendance');});
+// atn_except.addEventListener('click',()=>{loadPage('attendance_except');});
