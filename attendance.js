530d95efdbf49b7e43d27d862b2c17e28cc0d81b
s0948048 <chenti6508@gmail.com>
2024-08-22 17:04:47 +0800

    完成假勤考勤排版，需要員工面板首頁，需求：關閉表單功能

--- attendance.js
+++
@@ -1,9 +0,0 @@
-// const atn_index = document.getElementById('atn_list');
-// const atn_except = document.getElementById('see_except_data');
-
-
-
-
-
-// atn_index.addEventListener('click',()=>{loadPage('attendance');});
-// atn_except.addEventListener('click',()=>{loadPage('attendance_except');});
